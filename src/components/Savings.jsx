8335de4ab96f0496daf5eec24ccd54a148a30f2e
DavidF-U <dfrancisumoren@gmail.com>
2024-11-15 19:17:37 +0100

    UPDATED LIST components

--- src/components/Savings.jsx
+++ src/components/Savings.jsx
@@ -1,9 +1,35 @@
 import {SAVINGS} from '../constants';
-
+import { useState } from 'react';
+import { MdKeyboardArrowDown, MdKeyboardArrowUp } from "react-icons/md";
 
 function Savings() {
+  const [isOpen , setIsOpen] = useState(false)
+
   return (
-    <div>Savings</div>
+
+    <section id="" className=' grid grid-cols-1 items-center '>
+    {SAVINGS.map((general, index) => (
+<div  className=' relative px-28'>
+    <button onClick={() => setIsOpen((prev) => !prev )} key= {index}  className=' w-full  py-5 cursor-pointer appearance-none '>
+
+      <div className='flex justify-between items-center'>
+          <p className='text-gray-400'>{general.no}</p>
+          <h2 className='text-left max-sm:pl-3 md:pl-5'>{general.id}</h2>
+          <div className='text-neutral-950 text-lg rounded-sm grid justify-end'> 
+           {!isOpen ? (<MdKeyboardArrowDown />): ( <MdKeyboardArrowUp />)}
+          </div>
+      </div>
+    </button>
+
+        {isOpen && ( <div key={index} className=' overflow-hidden  text-left justify-center text-gray-500 px-5 max-md:text-sm md:px-10'>{general.description}</div> 
+                    )}
+        <div className='border-b-2 my-5' />
+ </div>
+
+))}
+
+    </section>
+
   )
 }
 
